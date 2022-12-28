0a59a4c7e9342c95f0e9434005fcf042cefeb7e9
EladRobin <118920124+EladRobin@users.noreply.github.com>
2022-12-28 19:39:57 +0200

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import FirstLayout from './components/firstLayout';
+
+function App() {
+  return (
+    <div >
+      <FirstLayout/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/layout/header/header.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Header = () => {
+  return (
+    <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
+    <div className="container">
+      <Link className="navbar-brand" to={'/'}>Start React</Link>
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          <li className="nav-item">
+            <Link className="nav-link active" aria-current="page" to={'/'}>Home</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to={'/page1'}>page1</Link>
+          </li>
+        
+          <li className="nav-item">
+            <Link className="nav-link" to={'/page2'}>page2</Link>
+          </li>
+        </ul>
+       
+      </div>
+    </div>
+  </nav>
+  )
+}
+
+export default Header
